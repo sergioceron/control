0ff8e7eda7f2196faf3e90b5a3413a4e43d29702
Sergio Ceron <sceronf@gmail.com>
2014-09-12 19:45:40 -0500

    Profesor Wizard & Parsley i18n

--- src/mx/ipn/cidetec/virtual/utils/CustomFormRenderer.java
+++ src/mx/ipn/cidetec/virtual/utils/CustomFormRenderer.java
@@ -1,7 +1,6 @@
 package mx.ipn.cidetec.virtual.utils;
 
 import com.sun.faces.renderkit.html_basic.FormRenderer;
-import com.sun.faces.renderkit.html_basic.TextRenderer;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -18,35 +17,52 @@ import java.io.IOException;
  */
 public class CustomFormRenderer extends FormRenderer {
 
-	private static final String[] attributes = new String[]{
+    private static final String[] attributes = new String[]{
 			"data-parsley-validate"
 	};
 
-	@Override
+    public CustomFormRenderer() {
+        super();
+    }
+
+    @Override
+    public void decode(FacesContext context, UIComponent component) {
+        super.decode(context, component);
+    }
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+        final ResponseWriter originalResponseWriter = context.getResponseWriter();
+        context.setResponseWriter(new ResponseWriterWrapper() {
+
+            @Override
+            public ResponseWriter getWrapped() {
+                return originalResponseWriter;
+            }
+
+            @Override
+            public void startElement(String name, UIComponent component) throws IOException {
+                super.startElement(name, component);
+                if ("form".equalsIgnoreCase(name)) {
+                    for (String attr : attributes) {
+                        final String value = (String) component.getAttributes().get(attr);
+                        if (value != null) {
+                            super.writeAttribute(attr, value, attr);
+                        }
+                    }
+                }
+            }
+        });
+        super.encodeBegin(context, component);
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        super.encodeEnd(context, component);
+    }
+
+    @Override
 	protected void getEndTextToRender(FacesContext context, UIComponent component, String currentValue) throws IOException {
-		final ResponseWriter originalResponseWriter = context.getResponseWriter();
-		context.setResponseWriter(new ResponseWriterWrapper() {
-
-			@Override
-			public ResponseWriter getWrapped() {
-				return originalResponseWriter;
-			}
-
-			@Override
-			public void startElement(String name, UIComponent component) throws IOException {
-				super.startElement(name, component);
-				if ("form".equalsIgnoreCase(name)) {
-					for (String attr : attributes) {
-						final String value = (String) component.getAttributes().get(attr);
-						if (value != null) {
-							super.writeAttribute(attr, value, attr);
-						}
-					}
-				}
-			}
-		});
-
-		super.getEndTextToRender(context, component, currentValue);
-		context.setResponseWriter(originalResponseWriter);
+        super.getEndTextToRender(context, component, currentValue);
 	}
 }
